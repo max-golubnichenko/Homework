77f216693dd1126c0afe4e48f17da7f0de5c57fe
Max <pargol2999@gmail.com>
2022-08-22 20:50:46 +0300

    fix bugs

--- lesson_6/script.js
+++ lesson_6/script.js
@@ -1,24 +1,23 @@
-const COUNT_OF_WORDS = 3;
-let word;
-let finalStr = ``;
-let format;
-let questionUser;
-
-questionUser = confirm(`Tell me three most important words 💚`);
+let questionUser = confirm(`Tell me three most important words 💚`);
 
 if (questionUser) {
 
+    const COUNT_OF_WORDS = 3;
+    let word;
+    let finalStr = ``;
+    let format;
+
     for (let i=1; i<=COUNT_OF_WORDS; i++) {
 
-        word = prompt(`Enter word #${i}`);
-        if (word) word = word.replaceAll(` `,``);
-
-        while (!word || word.match(/\d/)) {
+        do{
             word = prompt(`Enter word #${i}`);
             if (word) word = word.replaceAll(` `,``);
-        }
+        } while (!word || word.match(/\d/));
 
-        format = prompt(`Choose type of transformation for "${word}": uppercase|lowercase|capitalize`);
+        do{
+            format = prompt(`Choose type of transformation for "${word}": uppercase|lowercase|capitalize`);
+            if (format) format = format.replaceAll(` `,``);
+        } while (!format || (format !== 'uppercase' && format !== 'lowercase' && format !== 'capitalize'));
 
         if (format === `uppercase`) {
             format = format.replaceAll(` `,``);
@@ -31,11 +30,6 @@ if (questionUser) {
             word = word.charAt(0).toUpperCase() + word.slice(1);
         }
 
-        while (!(format === 'uppercase' || format === 'lowercase' || format === 'capitalize')) {
-            format = prompt(`Choose type of transformation for "${word}": uppercase|lowercase|capitalize`);
-            if (format) format = format.replaceAll(` `,``);
-        }
-
         finalStr += word;
 
         if (i === COUNT_OF_WORDS) {
@@ -44,11 +38,13 @@ if (questionUser) {
             finalStr += ` `;
         }
     }
+
+    console.log(finalStr);
+
 } else {
     alert(`Goodbye!`);
 }
 
-console.log(finalStr);
 
 
 
