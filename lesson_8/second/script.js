93d5faf7044eb2d20729b70b96570ca7a5ca95df
Max <pargol2999@gmail.com>
2022-08-29 13:06:37 +0300

    fix bugs

--- lesson_8/second/script.js
+++ lesson_8/second/script.js
@@ -3,18 +3,10 @@ let native = ['York','Of'];
 let destination = ['Poltava','In'];
 const COLORS = ['red', 'orange', 'yellow', 'green', 'blue', 'darkblue', 'purple'];
 
-let rainbow = hero.concat(native, destination);
-rainbow.reverse();
-rainbow.unshift('Richard','Gave','Battle');
-[rainbow[4], rainbow[7]] = [rainbow[7], rainbow[4]];
-rainbow.pop();
-[rainbow[3], rainbow[5]] = [rainbow[5], rainbow[3]];
-[rainbow[4], rainbow[6]] = [rainbow[6], rainbow[4]];
-[rainbow[1], rainbow[3]] = [rainbow[3], rainbow[1]];
-[rainbow[2], rainbow[4]] = [rainbow[4], rainbow[2]];
-rainbow.pop();
-rainbow.push(`Vain`);
-console.log(rainbow);
+let rainbow = destination.concat(native, hero).reverse();
+rainbow.splice(0,1,`Richard`);
+rainbow.splice(3, 0, `Gave`, `Battle`);
+rainbow.splice(-1,1,`Vain`);
 
 document.write(`<div style="display: flex; justify-content: space-between; margin: 0 auto; width: 500px;">`);
 
